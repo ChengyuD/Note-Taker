ce1f077d72031f1ed03283101cd8d4aa538fa550
Chengyu Dai <aacherythdai@gmail.com>
2021-06-01 22:16:06 -0700

    commit on notetaker

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.jason());
-app.use(express.static(_dirname));
+app.use(express.static(__dirname));
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, function() {
-    console.log(`App listening on PORT: ${PORT}`);
-});
+app.listen(PORT, () => console.log(`App listening on PORT: ${process.env.PORT}`));
